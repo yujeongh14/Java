c1fc78b30584aeeed85eb2fea4e9ec043a108a8b
yujeonghwang <syjayj@gmail.com>
2024-04-19 23:38:44 +0900

    정수타입

---
+++ ByteExample.java
@@ -0,0 +1,20 @@
+package ch02.sec02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		// 정수 타입의 메모리 크기		
+		// 1byte = 8bit
+		// byte : 1byte
+		
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 30;
+		byte var4 = 127;
+		// byte m5 = 128; // 컴파일 에러 Type mismatch: cannot convert from int to byte
+					      // (담을수 없는 숫자, 127까지 가능)
+		// System.out.println(m5);
+		
+	}
+
+}
