7a44fe9c0846e05dae85584390cbc6b69c6cf3be
yujeonghwang <syjayj@gmail.com>
2024-05-13 20:09:00 +0900

    NullPointerException

---
+++ NullPointerExceptionExample.java
@@ -0,0 +1,13 @@
+package ch05.sec01;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		// null: 참조 타입 변수가 아직 번지를 저장하지 않고 있다는 뜻. null을 값으로 가짐.
+		
+		int[] intArray = null;
+		//intArray[0] = 10;; //
+
+	}
+
+}
