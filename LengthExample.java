b2a64a122323f4799abf03b63c083bac30555a01
yujeonghwang <syjayj@gmail.com>
2024-05-15 20:31:35 +0900

    Length

---
+++ LengthExample.java
@@ -0,0 +1,18 @@
+package ch05.sec05;
+
+public class LengthExample {
+
+	public static void main(String[] args) {
+		// length : 문자열의 길이를 반환
+		
+		String ssn = "9506241230123";
+		int length = ssn.length();
+		if(length == 13) {
+			System.out.println("주민등록번호 자릿수가 맞습니다.");
+		} else {
+			System.out.println("주민등록번호 자릿수가 틀립니다.");
+		}
+
+	}
+
+}
